76f748a5ef72f2f7bf4459d5b1bc3a8634ab7c38
Son Le <son.le@hdwebsoft.co>
2020-12-12 15:50:49 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,6 +20,43 @@ Route::get('/', function () {
 Route::get('/products', function () {
     return 'get success';
 });
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => 3,
+        'redirect_uri' => 'http://laravel-vue.us/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+    ]);
+
+    return redirect('http://laravel-vue.us/oauth/authorize?'.$query);
+});
+
+Route::get('/callback', function (Request $request) {
+    $state = $request->session()->pull('state');
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class
+    );
+
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://laravel-vue.us/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => 3,
+            'client_secret' => 'HxEB7QqTbOgkpEv2v4ONiZbEyIUf9ZiknBg2e0Lg',
+            'redirect_uri' => 'http://laravel-vue.us/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
